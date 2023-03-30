22476717fa8d94c3546df01f724424bd237cec76
Jacob <jacobwroberts33@gmail.com>
2023-03-30 15:45:56 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// findMatching function
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// fuzzyMatch function
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.startsWith(query));
+}
+
+// matchName function
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name === query);
+}
